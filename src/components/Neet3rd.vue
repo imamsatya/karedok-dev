a46a523d08167fb796cd6e178a1675764028ad25
imamsatya <imamsatyawedhatama@gmail.com>
2023-09-18 22:08:40 +0700

    Update Minor

--- src/components/Neet3rd.vue
+++ src/components/Neet3rd.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="full-page purple-background">
 
-        <!-- <div class="square-full"></div> -->
+        <!-- <div class="square-full" ></div> -->
+        <!-- tes -->
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#30344f" fill-opacity="1" d="M0,288L48,282.7C96,277,192,267,288,240C384,213,480,171,576,160C672,149,768,171,864,170.7C960,171,1056,149,1152,133.3C1248,117,1344,107,1392,101.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
         <div class="grid grid-cols-12 gap-4 ">
             <div class="col-start-4 col-end-10  flex flex-col items-center">
